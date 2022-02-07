9dbcddd82a842cf5942d1eaa4b528a5c0539cb53
Misha <borisov.mikhail90@ya.ru>
2022-02-07 15:37:00 +0300

    Готово

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Main {
-    static Scanner scanner=new Scanner(System.in);
+public class Application {
+    static Scanner scanner = new Scanner(System.in);
     static String month;
 
     public static void viewMenu() {
@@ -13,11 +13,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        StepTracker stepTracker=new StepTracker();
-        viewMenu();
+        StepTracker stepTracker = new StepTracker();
         while (true) {
-            int numMenu=scanner.nextInt();
-            if (numMenu==1) {
+            viewMenu();
+            int numMenu = scanner.nextInt();
+            if (numMenu == 1) {
                 while (true) {
                     System.out.println("Про какой месяц речь");
                     month = scanner.next();
@@ -31,39 +31,24 @@ public class Main {
                 int numDay = scanner.nextInt();
                 System.out.println("Сколько шагов вы прошли за этот день");
                 int quantityOfSteps = scanner.nextInt();
-                stepTracker.writeActivity(month, numDay,quantityOfSteps);
+                stepTracker.writeActivity(month, numDay, quantityOfSteps);
 
-            } else if (numMenu==2) {
+            } else if (numMenu == 2) {
                 System.out.println("За какой месяц считать");
                 String monthChecked = scanner.next();
                 stepTracker.viewActivity(monthChecked);
-            } else if (numMenu==3) {
-            } else if (numMenu==4) {
+            } else if (numMenu == 3) {
+                System.out.println("Какую ежедневную цель хотите установить");
+                int newGoal = -1;
+                while (newGoal < 0) {
+                    System.out.println("Введите положительное значение");
+                    newGoal = scanner.nextInt();
+                    stepTracker.setDayGoal(newGoal);
+                }
+            } else if (numMenu == 4) {
                 break;
             }
         }
 
     }
 }
-    /*
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите год:");
-        int year = scanner.nextInt();
-
-        if (isLeapYear(year)) {
-            System.out.println("Год високосный, день программиста отмечаем 12.09." + year);
-
-        } else {
-            System.out.println("Год не високосный, день программиста отмечаем 13.09." + year);
-        }
-
-    }
-
-
-    public static boolean isLeapYear(int year) {
-        return (year % 100 != 0 && year % 4 == 0 || year % 400 == 0);
-    }
-
-}
-*/
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -2,55 +2,60 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class StepTracker {
-    private HashMap <String, Integer []> monthSteps = new HashMap<>();
+    private HashMap<String, int[]> monthSteps = new HashMap<>();
     Converter converter = new Converter();
-    int dayGoal = 10000;
-    int bestSeries;
-    StepTracker () {
-        for (Months month:Months.values()) {
-            monthSteps.put(String.valueOf(month), new Integer[30]);
+    private int dayGoal = 1000;
+    private int bestSeries;
+
+    StepTracker() {
+        for (Months month : Months.values()) {
+            monthSteps.put(String.valueOf(month), new int[30]);
         }
     }
 
-    void viewActivity (String month) {
-        int stepsOfMonth=0;
-        int activDay=0;
-        System.out.println("Статистика за месяц "+month+" :");
-        for (int i=0;i<monthSteps.get(month).length;i++) {
-            System.out.print((i+1) +" день:"+monthSteps.get(month)[i]);
-            if (monthSteps.get(month)[i]!=null) {
-                stepsOfMonth+=monthSteps.get(month)[i];
-                activDay++;
-            }
-            if (i>0) {
-
-            }
-        }
-        System.out.println("Количество шагов за месяц - "+stepsOfMonth);
-        System.out.println("Среднее количество шагов - "+ stepsOfMonth/activDay );
-        System.out.println("За месяц поройдено "+converter.convertDistance(stepsOfMonth)+" км");
-        System.out.println("За месяц сожжено "+converter.convertEnergy(stepsOfMonth)+" калорий");
-    }
-
-    void writeActivity (String monthName, int day, int steps) {
-                monthSteps.get(monthName)[day-1]=steps;
-    }
-
-    boolean checkMonth (String month) {
+    boolean checkMonth(String month) {
         return monthSteps.containsKey(month);
     }
-    
-    /*void viewActivity (String monthName, int numCheckedDay) {
-        Integer[] selectedMonth = monthSteps.get(monthName);
-        System.out.println("Количество пройденных шагов по дням :");
-        for (int i=0;i<selectedMonth.length;i++) {
-            System.out.print((i+1)+" день: "+selectedMonth[i]);
-        }*/
 
-
-
-        //System.out.println(monthName + " за " + numCheckedDay + " число прошел " + monthSteps.get(monthName)[numCheckedDay] + " шагов");
+    void writeActivity(String monthName, int day, int steps) {
+        monthSteps.get(monthName)[day - 1] = steps;
     }
+
+    void viewActivity(String month) {
+        int stepsOfMonth = 0;
+        int activDay = 0;
+        System.out.println("Статистика за месяц " + month + " :");
+        for (int i = 0; i < monthSteps.get(month).length; i++) {
+            System.out.print((i + 1) + " день:" + monthSteps.get(month)[i] + " ");
+            stepsOfMonth += monthSteps.get(month)[i];
+            activDay++;
+        }
+        System.out.println("\nКоличество шагов за месяц - " + stepsOfMonth);
+        System.out.println("Среднее количество шагов - " + stepsOfMonth / activDay);
+        System.out.println("За месяц поройдено " + converter.convertDistance(stepsOfMonth) + " км");
+        System.out.println("За месяц сожжено " + converter.convertEnergy(stepsOfMonth) + " калорий");
+        System.out.println("Лучшая серия составила " + findSeries(monthSteps.get(month)) + " дней");
+    }
+
+    int findSeries (int [] oneMonth) {
+        int localSerisesOfActivity = 1;
+        for (int i=0;i<(oneMonth.length-1);i++) {
+            if (oneMonth[i] >= dayGoal && oneMonth[i+1] >= dayGoal) {
+                localSerisesOfActivity++;
+            } else if (oneMonth[i]>dayGoal&&oneMonth[i+1] < dayGoal) {
+                localSerisesOfActivity = 1;
+            }
+            if (localSerisesOfActivity>bestSeries) {
+                bestSeries = localSerisesOfActivity;
+            }
+        }
+        return bestSeries;
+    }
+
+    public void setDayGoal(int dayGoal) {
+        this.dayGoal = dayGoal;
+    }
+}
 
 
 
